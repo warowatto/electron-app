b5c46b5d8f9fd185365ae8958f5399013de8cb54
yonghee kim <kyh1028@gmail.com>
2019-12-25 20:19:19 +0900

    version update

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
             releaseType: "release",
             private: false,
             protocol: "https",
-            token: "3f29f540a9c47679ac0dfc53ab3cf2006282748d"
+            token: "c7efdfbfbfc84d6fe3e46dea9b1d118ea44b95df"
           }
         ]
       }
